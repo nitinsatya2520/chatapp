ecc2f87825dd70a1c3e5ab194ebb7f89e7eb76cb
nitinsatya <nitinsatya656gmail.com>
2024-09-10 00:31:39 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://localhost:4000');
+const socket = io('https://chatserver-psi.vercel.app/');
 
 function App() {
   const [message, setMessage] = useState('');
