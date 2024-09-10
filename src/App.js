602dfb5d19d4e66ebec3ffeda76b6250bf3ef3aa
nitinsatya <nitinsatya656gmail.com>
2024-09-10 07:51:57 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('https://chatserver-psi.vercel.app/');
+const socket = io('https://chatserver-psi.vercel.app', {
+  transports: ['websocket', 'polling'],  // Ensure fallback to polling in case WebSocket fails
+  withCredentials: true
+});
+
 
 function App() {
   const [message, setMessage] = useState('');
