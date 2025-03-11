d3cd00408fc87615b3965cfc31b3bd3332765361
nitinsatya <nitinsatya656gmail.com>
2025-03-11 22:08:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import Login from './Login';
-import VideoCall from './VideoCall';
-import VideoMeeting from './VideoMeeting';
+
 function Chat() {
   const [message, setMessage] = useState('');
   const [chat, setChat] = useState([]);
@@ -136,7 +135,7 @@ function Chat() {
             <div className="navbar">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
-      <Link to="/video-call">Video Call</Link>
+      
       </div>
           </div>
         ) : (
@@ -214,8 +213,6 @@ function App() {
           <Route path="/login" element={<Login setUsername={setUsername} setIsLoggedIn={setIsLoggedIn} />} />
           <Route path="/" element={isLoggedIn ? <Chat username={username} /> : <Login setUsername={setUsername} setIsLoggedIn={setIsLoggedIn} />} />
           <Route path="/about" element={<About />} />
-          <Route path="/video-call" element={<VideoCall username={username} />} />
-          <Route path="/video-meeting" element={<VideoMeeting />} />
         </Routes>
       </div>
     </Router>
